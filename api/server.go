1baa638bcc2bd320eb8d4c8e0840db05a5d33ba7
hilariousz <andriano.abeh6@gmail.com>
2022-06-24 01:32:04 +0800

    server init

---
+++ api/server.go
@@ -0,0 +1,43 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+	db "github.com/hilariousz/simplebank/db/sqlc"
+)
+
+// Server serves HTTP request for banking service
+type Server struct {
+	store  db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and routing
+func NewServer(store db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// Use currency validator.go
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	router.POST("/transfers", server.createTransfer)
+
+	server.router = router
+	return server
+}
+
+// Start runs the HTTP server on specific address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
