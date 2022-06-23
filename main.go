e973831f5de2d45934ae43667a57c4e7da2368c4
hilariousz <andriano.abeh6@gmail.com>
2022-06-24 01:43:38 +0800

    init server

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/hilariousz/simplebank/api"
+	db "github.com/hilariousz/simplebank/db/sqlc"
+	"github.com/hilariousz/simplebank/util"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("Cannot connect to database:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("Cannot start server:", err)
+	}
+}
